a221c57a647d33b2f6c35205eab6f8daaf11a31a
seogineer <dgseo8981@gmail.com>
2022-12-30 15:42:06 +0900

    feat: 프로젝트 생성 및 회원 기능 추가

---
+++ src/test/java/com/seogineer/marketborojointest/MarketboroJoinTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seogineer.marketborojointest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketboroJoinTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
