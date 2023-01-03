28398384441a67e4920767b9d47cce4650ecc810
seogineer <dgseo8981@gmail.com>
2023-01-03 17:58:35 +0900

    refactor: 캐스팅 문제 해결

--- src/main/java/com/seogineer/marketborojointest/domain/MemberRepository.java
+++ src/main/java/com/seogineer/marketborojointest/domain/MemberRepository.java
@@ -1,6 +1,10 @@
 package com.seogineer.marketborojointest.domain;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    @Query("SELECT m FROM Member m LEFT JOIN FETCH m.reserves")
+    Optional<Member> findById(Long memberId);
 }
